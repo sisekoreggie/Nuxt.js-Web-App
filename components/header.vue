29c9c00ff59d1e0dd123ababd7acb486650af0cd
sisekoreggie <3443307@myuwc.ac.za>
2020-03-09 14:35:28 +0200

    Add request a demo link and styled buttons on header

--- components/header.vue
+++ components/header.vue
@@ -19,8 +19,8 @@
           <b-nav-item href="#">About</b-nav-item>
           <b-nav-item href="#">Contact</b-nav-item>
           <b-nav-item href="#">Blog</b-nav-item>
-          <b-nav-item href="#">Login</b-nav-item>
-          <b-nav-item href="#">Request A Demo</b-nav-item>
+          <b-nav-item id="login-button" href="#">Login</b-nav-item>
+          <b-nav-item id="demo-request-button" href="/signup">Request A Demo</b-nav-item>
         </b-navbar-nav>
       </b-navbar>
     </div>
@@ -36,5 +36,15 @@ export default {
 <style scoped>
 .dropdown-image {
   max-height: 1.5rem;
+}
+#demo-request-button {
+  background-color: #0178bc;
+  border: 1px solid #0178bc;
+  border-radius: 0.5rem;
+  margin-left: 1rem;
+}
+#login-button {
+  border: 1px solid white;
+  border-radius: 0.5rem;
 }
 </style>
