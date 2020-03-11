85a94668c546207715d6714d210d8ea144bb80d1
sisekoreggie <3443307@myuwc.ac.za>
2020-03-11 11:07:28 +0200

    Added calculations page

---
+++ pages/calculations.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="calculations">
+    <div class="light-grey-row">
+      <h1>Calculator</h1>
+      <p>Use any of the following calculators</p>
+    </div>
+    <div>
+      <b-container>
+        <b-row>
+          <h2>Add/Subtract/Divide/Multiply Numbers</h2>
+        </b-row>
+        <b-row>
+          <b-col>
+            <form class="form-inline" @submit.prevent="addNumbers">
+              <div class="form-group mb-2">
+                <label for="first-number" class="sr-only">First Number</label>
+                <input
+                  type="number"
+                  v-model="firstNumber"
+                  class="form-control"
+                  id="first-number"
+                  placeholder="First Number"
+                />
+              </div>
+              <div class="form-group mx-sm-3 mb-2">
+                <label for="second-number" class="sr-only">Second Number</label>
+                <input
+                  type="number"
+                  v-model="secondNumber"
+                  class="form-control"
+                  id="second-number"
+                  placeholder="Second Number"
+                />
+              </div>
+            </form>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <div>
+              <button v-on:click="addNumbers" class="btn btn-primary mb-2">Add</button>
+              <button v-on:click="subtractNumbers" class="btn btn-primary mb-2">Subtract</button>
+              <button v-on:click="multiplyNumbers" class="btn btn-primary mb-2">Multiply</button>
+              <button v-on:click="divideNumbers" class="btn btn-primary mb-2">Divide</button>
+            </div>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <p>Total: {{this.total}}</p>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculations',
+  data() {
+    return {
+      firstNumber: 0,
+      secondNumber: 0,
+      total: 0
+    }
+  },
+  head() {
+    return {
+      title: `Calculations | ${process.env.BasePageTitle}`
+    }
+  },
+  methods: {
+    addNumbers() {
+      this.total = parseInt(this.firstNumber) + parseInt(this.secondNumber)
+      return this.total
+    },
+    subtractNumbers() {
+      this.total = parseInt(this.firstNumber) - parseInt(this.secondNumber)
+      return this.total
+    },
+    multiplyNumbers() {
+      this.total = parseInt(this.firstNumber) * parseInt(this.secondNumber)
+      return this.total
+    },
+    divideNumbers() {
+      this.total = parseInt(this.firstNumber) / parseInt(this.secondNumber)
+      return this.total
+    }
+  }
+}
+</script>
