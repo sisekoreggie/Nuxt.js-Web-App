1f0dc48338256bb2ba5a26efd1afab50d9a6b3c6
sisekoreggie <3443307@myuwc.ac.za>
2020-03-12 10:45:18 +0200

    Moved test files to different folders

---
+++ test/integration/contact.spec.js
@@ -0,0 +1,8 @@
+import Contact from '@/pages/contact.vue';
+import { mount } from '@vue/test-utils';
+
+describe("integration test for contact page", () => {
+    test("", () => {
+        
+    });
+});
