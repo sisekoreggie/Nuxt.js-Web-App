6de24c9c20923a2804988db63d3e036088adf5ff
sisekoreggie <3443307@myuwc.ac.za>
2020-03-17 09:48:38 +0200

    Added first ava test

--- test/unit/calculations.test.js
+++ test/unit/calculations.test.js
@@ -1,5 +1,11 @@
-const test = require("ava");
+import test from 'ava';
+import { shallowMount } from '@vue/test-utils';
+import Calculations from '../../pages/calculations.vue';
+import Vue from 'vue';
 
-test("foo", t => {
+Vue.config.ignoredElements = [/b-/];
+const wrapper = shallowMount(Calculations);
+
+test("passing test", t => {
 	t.pass();
 });
